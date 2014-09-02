1bc3115f52f78226cb6935b94407a43ad56291f4
Miguel Gil <migilgil21@gmail.com>
2014-09-02 14:40:21 -0430

    migration preguntas
    Migracion

---
+++ application/modules/preguntas/migrations/004_Install_new_columns.php
@@ -0,0 +1,56 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Migration_Install_new_columns extends Migration
+{
+	/**
+	 * The name of the database table
+	 *
+	 * @var String
+	 */
+	private $table_name = 'preguntas';
+
+	/**
+	 * The table's fields
+	 *
+	 * @var Array
+	 */
+	private $fields = array(
+
+		'preguntas_tipo_id' => array(
+			'type' => 'INT',
+			'constraint' => 11,
+			'null' => FALSE,
+		),
+		'orden' => array(
+			'type' => 'INT',
+			'constraint' => 11,
+			'null' => FALSE,
+		),
+	);
+
+	/**
+	 * Install this migration
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		$this->dbforge->add_column($this->table_name,$this->fields);
+	}
+
+	//--------------------------------------------------------------------
+
+	/**
+	 * Uninstall this migration
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		$this->dbforge->drop_column($this->table_name,'preguntas_tipo_id');
+		$this->dbforge->drop_column($this->table_name,'orden');
+	}
+
+	//--------------------------------------------------------------------
+
+}
